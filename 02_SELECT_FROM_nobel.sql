bbea8523326a0caf80595d68ec3576d4a2e20a17
Humberto <hdkaratechop@gmail.com>
2022-09-15 23:50:59 -0700

    Add answer 3

--- 02_SELECT_FROM_nobel.sql
+++ 02_SELECT_FROM_nobel.sql
@@ -9,3 +9,21 @@ SELECT winner
   FROM nobel
  WHERE yr = 1962
    AND subject = 'literature'
+
+-- 3.Show the year and subject that won 'Albert Einstein' his prize.
+SELECT yr, subject FROM nobel 
+WHERE winner ='Albert Einstein';
+
+-- 4.Give the name of the 'peace' winners since the year 2000, including 2000.
+
+-- 5.Show all details (yr, subject, winner) of the literature prize winners for 1980 to 1989 inclusive.
+
+-- 6. Show all details of the presidential winners:
+-- Theodore Roosevelt
+-- Thomas Woodrow Wilson
+-- Jimmy Carter
+-- Barack Obama
+
+
+
+-- 7.Show the winners with first name John
