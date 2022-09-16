ffe4d25013e5178f5fb27662b8ad4b29562c8673
Humberto <hdkaratechop@gmail.com>
2022-09-15 18:55:50 -0700

    add answer 2

--- 02_SELECT_FROM_nobel.sql
+++ 02_SELECT_FROM_nobel.sql
@@ -3,3 +3,9 @@
 SELECT yr, subject, winner
   FROM nobel
  WHERE yr = 1950
+ 
+--  2.Show who won the 1962 prize for literature.
+SELECT winner
+  FROM nobel
+ WHERE yr = 1962
+   AND subject = 'literature'
