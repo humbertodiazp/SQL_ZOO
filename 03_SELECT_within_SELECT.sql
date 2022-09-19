911af0bb0690af99c0428c273689a6d392b8bcb4
Humberto <hdkaratechop@gmail.com>
2022-09-18 20:57:51 -0700

    add answer 7

--- 03_SELECT_within_SELECT.sql
+++ 03_SELECT_within_SELECT.sql
@@ -39,5 +39,26 @@ WHERE continent = 'Europe'
 -- Which countries have a GDP greater than every country in Europe? 
 -- [Give the name only.] (Some countries may have NULL gdp values
 SELECT name 
-FROM world  
+FROM world  x
 WHERE gdp > ALL (SELECT gdp FROM world y WHERE continent = "Europe" and gdp > 0)
+
+-- In that query, world is the name of a table (or possibly a view). The x and y are sometimes called
+--  "table aliases" but the technical term is "correlation name." x and y can be used elsewhere in the query 
+--  to unambiguously reference which instance of the world table you're using in an expression. 
+--  This is important in the expression y.continent=x.continent. Think of a correlation name as a kind of pointer
+--  to a series of rows within a table. As the query runs, a query's WHERE clause is evaluated once per row. 
+--  So the correlation name references one row at a time.
+-- By analogy, you can think of it like this code, which may be more familiar to you:
+-- for (x = 0; x < 10; ++x) { 
+--   for (y = 5; y < 15; ++y) { 
+--     if (x == y) { print("match!"); } 
+--   } 
+-- } 
+-- But in SQL, instead of integers, x and y reference rows in a table.
+
+-- 7. Find the largest country (by area) in each continent, show the continent, the name and the area:
+SELECT continent, name, area FROM world x
+  WHERE area >= ALL
+    (SELECT area FROM world y
+        WHERE y.continent=x.continent
+          AND population>0)
