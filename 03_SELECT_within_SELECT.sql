240cfdc99001fd9a897c895a0890cbf6971f26d5
Humberto <hdkaratechop@gmail.com>
2022-09-18 19:53:32 -0700

    add answer 5

--- 03_SELECT_within_SELECT.sql
+++ 03_SELECT_within_SELECT.sql
@@ -24,3 +24,13 @@ SELECT name, population FROM world
 WHERE population > (SELECT population FROM world 
 WHERE name = 'United Kingdom') AND population < (SELECT  population FROM world WHERE name = 'Germany')
 
+-- 5. Germany (population 80 million) has the largest population of the countries in Europe.
+-- Austria (population 8.5 million) has 11% of the population of Germany. Show the name and the 
+-- population of each country in Europe. Show the population as a percentage of the population of Germany.
+SELECT name,
+    CONCAT(ROUND(100*population/(SELECT population
+                    FROM WORLD 
+                    WHERE name = 'Germany'))
+                    , '%')
+FROM world
+WHERE continent = 'Europe'
