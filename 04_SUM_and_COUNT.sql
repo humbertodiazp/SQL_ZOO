b4a17dc87dc8b75c4fd072dc58d5d9e11041e6f4
Humberto <hdkaratechop@gmail.com>
2022-09-21 21:33:57 -0700

    add answer 8, use having sum statement to identify unique continents.

--- 04_SUM_and_COUNT.sql
+++ 04_SUM_and_COUNT.sql
@@ -28,3 +28,9 @@ SELECT continent, COUNT(name)
   FROM world
 WHERE population >= 10000000
  GROUP BY continent
+
+--  8.List the continents that have a total population of at least 100 million.
+SELECT continent
+  FROM world
+ GROUP BY continent
+HAVING SUM(population)>100000000
