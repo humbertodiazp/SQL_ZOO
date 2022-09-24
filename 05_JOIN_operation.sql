a6c2896774e12b11031bde8d1657e25911d85e8f
Humberto <hdkaratechop@gmail.com>
2022-09-24 04:45:55 -0700

    Add answer 11, use groupby on multiple selections

--- 05_JOIN_operation.sql
+++ 05_JOIN_operation.sql
@@ -73,4 +73,11 @@ SELECT stadium, COUNT(player)
 From game JOIN goal ON id = matchid
 GROUP BY stadium
 
+11.For every match involving 'POL', show the matchid, date and the number of goals scored.
+SELECT matchid, mdate, COUNT(player) goals_scored
+FROM game
+JOIN goal ON matchid = id
+WHERE (team1 = 'POL' OR team2 = 'POL')
+GROUP BY matchid, mdate
+
 
