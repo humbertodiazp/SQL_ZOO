4b37ae249c2b13cfc4f7d7e261beca296f960f73
Humberto <hdkaratechop@gmail.com>
2022-09-24 01:52:13 -0700

    Add answer 5, use join to find goals made within a certain time.

--- 05_JOIN_operation.sql
+++ 05_JOIN_operation.sql
@@ -35,3 +35,10 @@ WHERE teamid = 'GER'
 SELECT team1, team2, player
   FROM game JOIN goal ON (id=matchid)
 WHERE player LIKE 'Mario%'
+
+-- 5. The table eteam gives details of every national team including the coach. 
+-- You can JOIN goal to eteam using the phrase goal JOIN eteam on teamid=id
+-- Show player, teamid, coach, gtime for all goals scored in the first 10 minutes gtime<0
+SELECT player, teamid, coach, gtime
+  FROM goal JOIN eteam ON(teamid=id)
+ WHERE gtime<=10
