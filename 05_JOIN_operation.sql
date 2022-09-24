4f2d2188badfbbb72d160513fab47ef9158d0e1c
Humberto <hdkaratechop@gmail.com>
2022-09-24 02:57:45 -0700

    Add answer 8, comnine JOIN, WHERE, and AND to find more specific data.

--- 05_JOIN_operation.sql
+++ 05_JOIN_operation.sql
@@ -57,3 +57,11 @@ SELECT player
   FROM goal JOIN game ON (matchid=id)
 WHERE stadium = 'National Stadium, Warsaw'
 
+-- 8. Show names of all players who scored a goal against Germany.
+SELECT DISTINCT player
+  FROM game JOIN goal ON matchid = id
+    WHERE (team1='GER' OR team2='GER') 
+AND teamid!= 'GER'
+
+
+
