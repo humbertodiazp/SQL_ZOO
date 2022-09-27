2cbb800f821be810d3ea31729d01710297217d42
Humberto <hdkaratechop@gmail.com>
2022-09-26 17:07:21 -0700

    Add answer 13, use left join to select all matches in the game table.

--- 05_JOIN_operation.sql
+++ 05_JOIN_operation.sql
@@ -86,4 +86,15 @@ SELECT matchid,mdate,COUNT(teamid)
  WHERE (teamid='GER')
 GROUP BY matchid,mdate
 
+-- 13. List every match with the goals scored by each team as shown.
+--  This will use "CASE WHEN" which has not been explained in any previous exercises.
+SELECT mdate,
+  team1,
+  SUM(CASE WHEN teamid=team1 THEN 1 ELSE 0 END) score1,
+  team2,
+  SUM(CASE WHEN teamid=team2 THEN 1 ELSE 0 END) score2
+  FROM game LEFT JOIN goal ON matchid = id 
+GROUP BY mdate,team1,team2
+;
+
 
