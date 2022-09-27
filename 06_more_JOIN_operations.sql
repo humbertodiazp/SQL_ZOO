500646dd0c496e786ddd501a216e7acedff21078
Humberto <hdkaratechop@gmail.com>
2022-09-26 17:56:36 -0700

    add answer 3, use LIKE to find star trek movies

--- 06_more_JOIN_operations.sql
+++ 06_more_JOIN_operations.sql
@@ -3,7 +3,14 @@ SELECT id, title
  FROM movie
  WHERE yr=1962
 
- 2. Give year of 'Citizen Kane'.
+--  2. Give year of 'Citizen Kane'.
  SELECT yr 
  FROM movie
  WHERE title='Citizen Kane'
+
+--  3.List all of the Star Trek movies, include the id, title and yr (all of these movies include the words Star Trek in the title). 
+-- Order results by year.
+SELECT id, title, yr
+FROM movie
+WHERE title LIKE 'STAR TREK%'
+ORDER BY yr
