b5a128e9e4b769b5eb45732da3ec957e16871626
Humberto <hdkaratechop@gmail.com>
2022-10-02 21:28:49 -0700

    add answer 14, use a group by and order by statement to find data.

--- 06_more_JOIN_operations.sql
+++ 06_more_JOIN_operations.sql
@@ -107,3 +107,14 @@ ON actorid = id
 WHERE ord = 1
 GROUP BY name
 HAVING COUNT(actorid) >= 15;
+
+-- 14. List the films released in the year 1978 ordered by the number of actors in the cast, then by title.
+
+SELECT title, COUNT(actorid)
+ FROM movie 
+JOIN casting ON movie.id = movieid
+ WHERE yr = 1978
+  GROUP BY title 
+ORDER BY COUNT(actorid) DESC, title
+
+-- 15. List all the people who have worked with 'Art Garfunkel'.
