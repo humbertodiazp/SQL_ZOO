f70dc3ce92e4740c8723d750286c850ed03eae69
Humberto <hdkaratechop@gmail.com>
2022-10-03 23:24:44 -0700

    Add answer 2, noting what data is missed by using inner join alone.

--- 07_using_NULL.sql
+++ 07_using_NULL.sql
@@ -2,3 +2,9 @@
 -- List the teachers who have NULL for their department.
 SELECT name FROM teacher 
 WHERE dept is NULL
+
+-- 2.
+-- Note the INNER JOIN misses the teachers with no department and the departments with no teacher.
+SELECT teacher.name, dept.name
+ FROM teacher INNER JOIN dept
+           ON (teacher.dept=dept.id)
