e4818201c1b7e2fa6c3c06b63efc3f8658e0dab1
Humberto <hdkaratechop@gmail.com>
2022-10-10 23:35:25 -0700

    add answer 2, use name to find id number

--- 08_SELF_JOIN.sql
+++ 08_SELF_JOIN.sql
@@ -1,3 +1,10 @@
-1.
-How many stops are in the database.
+-- 1.
+-- How many stops are in the database.
 SELECT COUNT(*) FROM stops
+
+-- 2.
+-- Find the id value for the stop 'Craiglockhart'
+SELECT id FROM stops
+WHERE name = 'Craiglockhart'
+
+
