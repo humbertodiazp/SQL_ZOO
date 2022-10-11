0a8d3f49c832f10bf4ad4c94b8dadaef41efbb24
Humberto <hdkaratechop@gmail.com>
2022-10-10 23:50:21 -0700

    Add answer 3, use join to find unique data

--- 08_SELF_JOIN.sql
+++ 08_SELF_JOIN.sql
@@ -7,4 +7,11 @@ SELECT COUNT(*) FROM stops
 SELECT id FROM stops
 WHERE name = 'Craiglockhart'
 
+-- 3.
+-- Give the id and the name for the stops on the '4' 'LRT' service.
+SELECT id, name
+FROM stops
+JOIN route ON id = stop 
+WHERE num = '4' 
+AND company = 'LRT'
 
