36c25e131a76f3f48a22aefde0356fde66f6ecc9
Humberto <hdkaratechop@gmail.com>
2022-10-16 13:24:19 -0700

    add asnwer 9, use self join on two tables and reverse order of stops

--- 08_SELF_JOIN.sql
+++ 08_SELF_JOIN.sql
@@ -58,3 +58,12 @@ JOIN stops AS bstop ON (b.stop = bstop.id)
 WHERE astop.name = 'Craiglockhart' 
 AND bstop.name = 'Tollcross'
 GROUP BY company, num
+
+-- 9.
+-- Give a distinct list of the stops which may be reached from 'Craiglockhart' by taking one bus, including 'Craiglockhart' itself, offered by the LRT company.
+-- Include the company and bus no. of the relevant services.
+SELECT DISTINCT bstop.name, a.company, a.num FROM
+	route AS a JOIN route AS b ON (a.company = b.company AND a.num = b.num)
+			   JOIN stops AS astop ON (a.stop = astop.id)
+			   JOIN stops AS bstop ON (b.stop = bstop.id)
+	WHERE astop.name = 'Craiglockhart'
