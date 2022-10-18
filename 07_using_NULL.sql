17fca39ad2e69eb9ea49f335c9ea05d063b49fc2
Humberto <hdkaratechop@gmail.com>
2022-10-17 23:55:39 -0700

    add answer 10, use additional condition with case statement for null val

--- 07_using_NULL.sql
+++ 07_using_NULL.sql
@@ -61,3 +61,14 @@ CASE
     ELSE 'Art'
 END AS 'DEPT TEXT'
 FROM teacher
+
+-- 10.
+-- Use CASE to show the name of each teacher followed by 'Sci' if the teacher is in dept 1 or 2, show 'Art' if the teacher's dept is 3 and 'None' otherwise.
+SELECT name,
+CASE
+    WHEN dept = 1 THEN "Sci"
+    WHEN dept = 2 THEN "Sci"
+    WHEN dept = 3 THEN 'Art'
+    ELSE 'None'
+END AS 'DEPT TEXT'
+FROM teacher
